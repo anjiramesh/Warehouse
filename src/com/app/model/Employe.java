cb4a53c297f8d092b3b749d4d110cae36dae54b7
Dhanamjaya K <Dhanamjaya K@DESKTOP-HPQTNE3>
2019-05-29 19:26:05 +0530

    Step #3 Add def const,set,get ,tostring

--- src/com/app/model/Employe.java
+++ src/com/app/model/Employe.java
@@ -3,5 +3,27 @@ package com.app.model;
 public class Employe {
 
 	private String empName;
+	private Double empSal;
+	public Employe() {
+		super();
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public Double getEmpSal() {
+		return empSal;
+	}
+	public void setEmpSal(Double empSal) {
+		this.empSal = empSal;
+	}
+	@Override
+	public String toString() {
+		return "Employe [empName=" + empName + ", empSal=" + empSal + "]";
+	}
+	
+	
 	
 }
